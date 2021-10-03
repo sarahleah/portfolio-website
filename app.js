3502f10e2b958dac445a6998896d0fa608f645fb
sarahandreas <sarah.andreas@gmail.com>
2021-10-03 15:07:06 +1100

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+let navBarListItems = document.querySelectorAll(".nav-bar__list--item")
+let navBarMarker = document.querySelector(".nav-bar__marker")
+let homeItem = document.querySelector(".nav-bar__list--item")
+
+navBarMarker.style.left = homeItem.offsetLeft + 'px'
+navBarMarker.style.width = homeItem.offsetWidth + 'px'
+
+function handleNavMarkerMovement(e) {
+	let selectedItem = e.target
+	let lastMarkerLeft = Number(navBarMarker.style.left.replace('px', ''))
+
+	if (lastMarkerLeft > selectedItem.offsetLeft) {
+		navBarMarker.classList.add('rightLean')
+	} else {
+		navBarMarker.classList.remove('rightLean')
+	}
+	
+	navBarMarker.style.left = selectedItem.offsetLeft + 'px'
+	navBarMarker.style.width = selectedItem.offsetWidth + 'px'
+}
+
+navBarListItems.forEach(item => item.addEventListener('mouseenter', handleNavMarkerMovement))
